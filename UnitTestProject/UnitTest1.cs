1753e162e33423d1dfe4c6cc1e712cd56af6bb5f
David Valverde Garro <Dav.Val.gar-77@hotmail.com>
2018-11-16 12:01:03 -0600

    Agregado proyecto de unit testing y agregados mas connection strings a DOA_SQL.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DashboardAccidentes.Negocio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //DAO_Carga y DAO_SQL tienen que ser publicas para poder correr esto
+            //En general cualquier clase invocada desde aqui necesita ser publica
+            //DAO_Carga dAO_Carga = new DAO_Carga();
+            //dAO_Carga.getDistritos("CURRIDABAT");
+        }
+    }
+}
