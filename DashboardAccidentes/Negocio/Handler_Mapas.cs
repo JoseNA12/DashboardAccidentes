2656961c41b338039092dabc1e51dcd8d9c809b3
David Valverde Garro <Dav.Val.gar-77@hotmail.com>
2018-11-17 17:05:30 -0600

    * Agregado esqueleto del handler de mapas.

---
+++ DashboardAccidentes/Negocio/Handler_Mapas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardAccidentes.Negocio
+{
+    public class Handler_Mapas
+    {
+        public DataTable procesarResultadosQuery(DataTable dt)
+        {
+            //Aqui va el procesamiento de LINQ
+            return null;
+        }
+
+        public void formatearCoordenadas()
+        {
+            //Aqui va lo del iteraror
+        }
+
+        public void realizarConsulta() //TODO no se si vamos a pasarle el query por un dto o por parametros separados
+        {
+            DAO_Query dao = new DAO_Query();
+            //DataTable datos = procesarResultadosQuery(dao.correrQueryDinamico());
+        }
+    }
+}
